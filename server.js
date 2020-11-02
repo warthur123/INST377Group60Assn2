539ed5071ae7bc2b0b61118570ddc3f901fb999e
Arthur <37461679+warthur123@users.noreply.github.com>
2020-11-02 18:36:41 -0500

    .

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js'
 import fetch from 'node-fetch';
 
 dotenv.config();
@@ -22,20 +21,20 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get(async (req, res) => {
+  .get((req, res) => {
     console.log('GET request detected');
     res.send(`Lab 5 for ${process.env.NAME}`);
   })
   .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
+    console.log('data from fetch', json);
     res.json(json);
-
-    console.log('POST request detected');
-    console.log('Fetch request data', json);
   });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
-
